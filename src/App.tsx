a4e7f83fc4740f45d757ac14efd477b7c193e096
itsyaboikris <kristoff_riley@outlook.com>
2020-05-19 21:03:19 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ChangeEvent, Component} from 'react';
+import {AxiosResponse} from "axios";
+import {MDBContainer, MDBCol, MDBRow} from "mdbreact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const axios = require('axios').default;
+
+class DynamicDropdown extends Component {
+
+    state = {
+        organisations: [],
+        branches: [],
+        selectedOne: "",
+        selectedTwo: ""
+    };
+
+    componentDidMount() {
+        this.getOrganisations();
+    };
+
+    async getOrganisations() {
+        await axios.get("http://localhost:3000/organisation/selectAllOrganisations")
+            .then((response: AxiosResponse) => {
+                this.setState({
+                    organisations: response.data.success.data
+                });
+            });
+    };
+
+    async getBranches(orgID: Number) {
+        await axios.post("http://localhost:3000/branch/selectAllBranchByOrgID",
+            {
+                "id": orgID
+            })
+            .then((response: AxiosResponse) => {
+                this.setState({
+                    branches: response.data.success.data
+                });
+
+            })
+    };
+
+    handleDropdownOne = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        this.setState({[event.target.name]: [event.target.value]});
+        this.getBranches(Number(event.target.value));
+    };
+
+    handleDropdownTwo = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        this.setState({[event.target.name]: [event.target.value]});
+    };
+
+
+    render() {
+        return (
+            <div className="App">
+                <MDBContainer>
+                    <MDBRow>
+                        <MDBCol xs="12" sm="12" md="12" lg="12">
+                            <h1>Dynamic Dependant Dropdowns</h1>
+                        </MDBCol>
+                    </MDBRow>
+                    <MDBRow>
+                        <MDBCol xs="12" sm="12" md="6" lg="6">
+                            <select onChange={this.handleDropdownOne} name="selectedOne" className="browser-default custom-select">
+                                <option>--Select Organisation--</option>
+                                {
+                                    this.state.organisations.map(
+                                        (organisation: any) =>
+                                            <option key={organisation.id} value={organisation.id}>
+                                                {organisation.name}
+                                            </option>
+                                    )
+                                }
+                            </select>
+                        </MDBCol>
+                        <MDBCol xs="12" sm="12" md="6" lg="6">
+                            <select onChange={this.handleDropdownTwo} name="selectedTwo" className="browser-default custom-select">
+                                <option>--Select Branches--</option>
+                                {
+                                    this.state.branches.map(
+                                        (branch: any) =>
+                                            <option key={branch.branch_id} value={branch.branch_id}>
+                                                {branch.branch_name}
+                                            </option>
+                                    )
+                                }
+                            </select>
+                        </MDBCol>
+
+                    </MDBRow>
+                </MDBContainer>
+            </div>
+        );
+    }
+
 }
 
-export default App;
+export default DynamicDropdown;
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
